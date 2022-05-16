d229dc2ee14f5813bfb03ee195aa4f19abe8f7e3
Johannes B <johannes.bock@live.de>
2022-05-16 13:15:09 +0200

    Regex Beginn

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Eingabeprüfung_T11_SS2
@@ -47,7 +48,8 @@ namespace Eingabeprüfung_T11_SS2
                 Console.Write("Das Passwort soll \n" +
                     "-mindestens acht Zeichen\n" +
                     "-mindestens eine Ziffer\n" +
-                    "-mindestens einen Groß- und einen Kleinbuchstaben enthalten.\n");
+                    "-mindestens einen Groß- und einen Kleinbuchstaben\n" +
+                    "-mindestens ein Sonderzeichen enthalten.\n");
                 do
                 {
                     Console.Write("Passwort eingeben:");
@@ -56,26 +58,45 @@ namespace Eingabeprüfung_T11_SS2
                     if (EnthältZiffern(password)) zähler++;
                     if (EnthältGroßbuchstaben(password)) zähler++;
                     if (EnthältKleinbuchstaben(password)) zähler++;
+                    if (EnthältSonderzeichen(password)) zähler++;
 
-                    if (zähler == 4) korrekt = true;
+                    if (zähler == 5)
+                    {
+                        korrekt = true;
+                        Console.WriteLine("Gültige Eingabe.");
+                    }
                     else
                     {
                         korrekt = false;
                         Console.WriteLine("Ungültige Eingabe!");
                     }
-                } while (korrekt==false);
+                } while (korrekt == false);
                 Console.WriteLine("Gutes Passwort.");
             }
         }
 
-        static bool EnthältZiffern(string testString)
+        private static bool EnthältSonderzeichen(string testString)
         {
-            bool ergebnis=false;
+            bool ergebnis = false;
             for (int i = 0; i < testString.Length; i++)
             {
-                if (Char.IsDigit(testString[i])) ergebnis = true;
+                if (!Char.IsLetterOrDigit(testString[i])) ergebnis = true;
             }
             return ergebnis;
+        }
+
+        static bool EnthältZiffern(string testString)
+        {
+            //bool ergebnis = false;
+            //for (int i = 0; i < testString.Length; i++)
+            //{
+            //    if (Char.IsDigit(testString[i])) ergebnis = true;
+            //}
+            //Regex reg = new Regex("^[0-9]+$");
+            //ergebnis = reg.IsMatch(testString);
+            //return ergebnis;
+
+            return Regex.IsMatch(testString, "^[0-9]+$");
         }
 
         static bool EnthältGroßbuchstaben(string testString)
